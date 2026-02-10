d3bfe0d6f0e42e151fe5353373d4a084aeafb25a
Anderson Trombini <anderson@viafast.com.br>
2026-02-10 10:22:04 -0300

    ajuste seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,10 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            TechnicalSeeder::class,
-            EquipmentSeeder::class,
-            ServiceOrderSeeder::class,
-            EquipmentServiceOrderSeeder::class,
+            // TechnicalSeeder::class,
+            // EquipmentSeeder::class,
+            // ServiceOrderSeeder::class,
+            // EquipmentServiceOrderSeeder::class,
         ]);
     }
 }
