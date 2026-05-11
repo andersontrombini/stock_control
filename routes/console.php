0cdb4724c30ab77d2d464615a1dd22187838be01
Anderson Trombini <anderson@viafast.com.br>
2026-05-11 20:38:24 -0300

    removendo envios automaticos

--- routes/console.php
+++ routes/console.php
@@ -4,11 +4,11 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('stock:check-low')->dailyAt('09:00');
+// Schedule::command('stock:check-low')->dailyAt('09:00');
 
-Schedule::command('report:monthly-service-orders')->lastDayOfMonth('17:59');
+// Schedule::command('report:monthly-service-orders')->lastDayOfMonth('17:59');
 
-Schedule::command('report:weekly-equipments')->mondays()->at('09:00');
+// Schedule::command('report:weekly-equipments')->mondays()->at('09:00');
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
